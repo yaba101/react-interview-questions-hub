2adb67e3989242dc9c52bdc9b0a2b06a64248fa4
Yeabsira Mekuria <yabumek46@gmail.com>
2023-10-24 20:05:22 +0300

    update introduction page

--- theme.config.tsx
+++ theme.config.tsx
@@ -1,18 +1,15 @@
-import React from 'react'
-import { DocsThemeConfig } from 'nextra-theme-docs'
+import React from "react";
+import { DocsThemeConfig } from "nextra-theme-docs";
 
 const config: DocsThemeConfig = {
-  logo: <span>My Project</span>,
+  logo: <span>React Interview Questions</span>,
   project: {
-    link: 'https://github.com/shuding/nextra-docs-template',
+    link: "https://github.com/yaba101/react-interview-questions",
   },
-  chat: {
-    link: 'https://discord.com',
-  },
-  docsRepositoryBase: 'https://github.com/shuding/nextra-docs-template',
+  docsRepositoryBase: "https://github.com/yaba101/react-interview-questions",
   footer: {
-    text: 'Nextra Docs Template',
+    text: "React Interview Questions",
   },
-}
+};
 
-export default config
+export default config;
