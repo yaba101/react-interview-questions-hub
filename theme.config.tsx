6e3d53268e2ae39fb62219a47b6b6ddd6652fda9
Yeabsira Mekuria <yabumek46@gmail.com>
2023-10-24 20:35:45 +0300

    update the site name and intro page

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,13 +2,13 @@ import React from "react";
 import { DocsThemeConfig } from "nextra-theme-docs";
 
 const config: DocsThemeConfig = {
-  logo: <span>React Interview Questions</span>,
+  logo: <span>React Interview Questions Hub</span>,
   project: {
     link: "https://github.com/yaba101/react-interview-questions",
   },
   docsRepositoryBase: "https://github.com/yaba101/react-interview-questions",
   footer: {
-    text: "React Interview Questions",
+    text: "React Interview Questions Hub",
   },
 };
 
