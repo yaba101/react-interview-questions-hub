5919969228b43fe8f2cbb64b7f0718954be77fa6
Yeabsira Mekuria <yabumek46@gmail.com>
2023-10-30 20:17:06 +0300

    add junior question about hooks rule

--- theme.config.tsx
+++ theme.config.tsx
@@ -2,7 +2,9 @@ import React from "react";
 import { DocsThemeConfig } from "nextra-theme-docs";
 
 const config: DocsThemeConfig = {
-  logo: <span>React Interview Questions Hub</span>,
+  logo: (
+    <span style={{ fontWeight: "bold" }}>React Interview Questions Hub</span>
+  ),
   project: {
     link: "https://github.com/yaba101/react-interview-questions",
   },
